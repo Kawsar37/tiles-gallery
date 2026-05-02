c229ed3b0c823bfc0863f4a042463848bfbb61ad
Kawsar37 <kawsarali750@gmail.com>
2026-05-02 15:27:18 +0600

    search function fixed

--- src/app/all-tiles/AllTiles.jsx
+++ src/app/all-tiles/AllTiles.jsx
@@ -8,8 +8,10 @@ export default function AllTiles({ tiles }) {
   const [text, setText] = useState("");
 
   const filteredTiles = tiles.filter((tile) =>
-    tile.title.toLowerCase().includes(text),
+    tile.title.toLowerCase().includes(text.toLowerCase()),
   );
+
+  console.log("filter tiles", text);
 
   return (
     <div className=" max-w-[85%] mx-auto mt-7 sm:mt-10">
@@ -26,18 +28,27 @@ export default function AllTiles({ tiles }) {
               value={text}
               onChange={(e) => setText(e.target.value)}
               className="w-70"
+              id="input-box"
               placeholder="Search..."
             />
-            <SearchField.ClearButton />
+            <SearchField.ClearButton onClick={() => setText("")} />
           </SearchField.Group>
         </SearchField>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-5">
-        {filteredTiles.map((tile) => (
-          <TileCard tile={tile} key={tile.id} />
-        ))}
-      </div>
+      {filteredTiles.length ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-5">
+          {filteredTiles.map((tile) => (
+            <TileCard tile={tile} key={tile.id} />
+          ))}
+        </div>
+      ) : (
+        <div className="min-h-[60vh] flex items-center justify-center">
+          <h3 className="text-2xl font-semibold text-blue-300">
+            No Tiles Found!
+          </h3>
+        </div>
+      )}
     </div>
   );
 }
